f5cfec4be74f491b28e7d07289680ca99951d5cb
Gabriel Silva <gsilva2510@hotmail.com>
2020-04-12 18:39:14 -0300

    change console message

--- TimeComplexity/Program.cs
+++ TimeComplexity/Program.cs
@@ -6,7 +6,7 @@ namespace TimeComplexity
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Please, check the tests project to see the magic.");
         }
     }
 }
