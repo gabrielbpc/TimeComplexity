536ed3f795a7588b9f1af048bf745db123dd1628
Gabriel Silva <gsilva2510@hotmail.com>
2020-04-12 22:11:59 -0300

    binary search implemented

---
+++ TimeComplexity.Tests/BinarySearchTest.cs
@@ -0,0 +1,37 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TimeComplexity.Exercises;
+
+namespace TimeComplexity.Tests
+{
+    [TestFixture]
+    public class BinarySearchTest
+    {
+        private Fixture fixture;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            fixture = new Fixture();
+        }
+
+        // Write a binary search that returns the index of the item.
+        // Binary search if time complexity O(log n).
+
+        [Test]
+        public void ShouldFindAIntegerIndexOnAnArray()
+        {
+            int[] array = fixture.CreateMany<int>(100).OrderBy(k => k).ToArray();
+            int position = new Random().Next(1, 100);
+
+            var result = BinarySearch.FindIndex(array, array[position]);
+
+            result.Should().BeOfType(typeof(int));
+            result.Should().Be(position);
+        }
+    }
+}
---
+++ TimeComplexity/Exercises/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeComplexity.Exercises
+{
+    public static class BinarySearch
+    {
+        public static int FindIndex(int[] array, int number) => Search(array, 0, array.Length, number);
+
+        private static int Search(int[] array, int left, int right, int number)
+        {
+            if (right >= left)
+            {
+                int mid = GetMiddle(left, right);
+
+                if (array[mid] == number)
+                    return mid;
+
+                if (array[mid] > number)
+                    return Search(array, left, mid - 1, number);
+
+                return Search(array, mid + 1, right, number);
+            }
+
+            return -1;
+        }
+
+        private static int GetMiddle(int left, int right) => left + (right - left) / 2;
+    }
+}
