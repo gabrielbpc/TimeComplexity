6f45add1cbb591cd581d06e0110136b92554c8cc
Gabriel Silva <gsilva2510@hotmail.com>
2020-04-13 00:12:50 -0300

    implementing gdc

---
+++ TimeComplexity.Tests/GreatestCommonDivisorTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TimeComplexity.Exercises;
+
+namespace TimeComplexity.Tests
+{
+    [TestFixture]
+    public class GreatestCommonDivisorTest
+    {
+        // The greatest common divisor is the larger positive integer that divides all numbers without giving a remainder.
+
+        [Test]
+        public void ShouldFindTheGDCValue()
+        {
+            var input = new int[] { 2, 4, 6, 8, 10 };
+
+            var output = GreatestCommonDivisor.FindGreatestCommonDivisor(input);
+
+            output.Should().Be(2);
+        }
+    }
+}
---
+++ TimeComplexity/Exercises/GreatestCommonDivisor.cs
@@ -0,0 +1,27 @@
+﻿namespace TimeComplexity.Exercises
+{
+    public static class GreatestCommonDivisor
+    {
+        public static int FindGreatestCommonDivisor(int[] array)
+        {
+            int result = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                result = Calculate(array[i], result);
+
+                if (result == 1)
+                    return 1;
+            }
+
+            return result;
+        }
+
+        private static int Calculate(int a, int b)
+        {
+            if (a == 0)
+                return b;
+
+            return Calculate(b % a, a);
+        }
+    }
+}
